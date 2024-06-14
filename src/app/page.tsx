2c9ca5aa7a7fbf259a9e50b61c25d123141f44a7
Neo-dash <neodash.km@gmail.com>
2024-06-15 00:55:18 +0100

    add schema and print document

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
 'use client'
 
 import { SignInButton, UserButton } from "@clerk/nextjs";
-import { Authenticated, Unauthenticated } from "convex/react";
+import { Authenticated, Unauthenticated, useMutation, useQueries, useQuery } from "convex/react";
 import { Content } from "next/font/google";
+import { api } from "../../convex/_generated/api";
+import { title } from "process";
 
 export default function Home() {
+
+
+  const documents = useQuery(api.documents.getDocuments);
+  const createDocument = useMutation(api.documents.createDocument);
+
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Unauthenticated>
@@ -12,6 +20,20 @@ export default function Home() {
       </Unauthenticated>
       <Authenticated>
         <UserButton />
+
+        <button onClick={() => {
+          createDocument({title: 'hello world'})
+
+
+        }}> Click Me</button>
+
+        {documents?.map((doc) => (
+
+          <div key={doc._id}>{doc.title}</div>
+        ))}
+
+
+
       </Authenticated>
     </main>
   );
