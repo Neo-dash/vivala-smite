accb88bb10ea9992f909d76f1fa0df6e580df490
Neo-dash <neodash.km@gmail.com>
2024-06-19 23:44:26 +0100

    lets try this

--- src/app/document-card.tsx
+++ src/app/document-card.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 import { Eye } from "lucide-react";
 import Link from 'next/link';  // Correct import for Next.js Link
 import { Card, CardHeader, CardTitle, CardContent, CardFooter, CardDescription } from "@/components/ui/card";
-
+//comment
 export function DocumentCard({ document }: { document: Doc<"documents"> }) {
     return (
         <Card>
